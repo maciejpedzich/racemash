deb3d07612506bd413e45ac6ab3742c149c6665d
Maciej Pędzich <contact@maciejpedzi.ch>
2023-05-23 10:05:36 +0200

    Install ESLint and Prettier plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    'prettier'
   ],
+  plugins: ['prettier'],
   rules: {
     'vue/multi-word-component-names': 'off',
-  },
-}
+    'prettier/prettier': 'error'
+  }
+};
